3c5229066efe9c8e0e232deff1ab16f083a4883f
KarlStevenRequinto <lerdrobott@gmail.com>
2025-03-27 20:16:30 +0800

    update packagelock

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const monaSans = Mona_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "PrepWise",
+  title: "AI Prep Coach",
   description: "An AI-powered platform for preparing for mock interviews",
 };
 
